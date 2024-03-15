e1173e6d6c30e2f6ae80a697b8bbafbc3cdb3997
mcastre1 <miguelcastrejongal@gmail.com>
2024-03-14 22:25:00 -0600

    Creating a project extension

---
+++ ksl_listings/scripts/background.js
@@ -0,0 +1,22 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "OFF",
+    });
+});
+
+const ksl_search = "https://classifieds.ksl.com/search/keyword"
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(ksl_search)) {
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState
+        });
+
+        console.log("yay!")
+    }
+});
