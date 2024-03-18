721a3519072634195695e80032bcd51ff6a3d729
mcastre1 <miguelcastrejongal@gmail.com>
2024-03-17 21:46:29 -0600

    Finding the price in each listing

--- ksl_listings/scripts/add_average_range.js
+++ ksl_listings/scripts/add_average_range.js
@@ -4,5 +4,7 @@ console.log("YAY!");
 console.log(listings);
 
 for (var i = 0; i < listings.length; i++) {
-    console.log(listings[i].querySelector(".item-info-title-link").textContent);
+    let title = listings[i].querySelector(".item-info-title-link").textContent
+    let price = listings[i].querySelector(".item-info-price").textContent
+    console.log("%s %s", title, price);
 }
